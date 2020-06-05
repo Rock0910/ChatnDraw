e7a4a7fa4ab725c9dc3cda164fbbc1fff5df694d
Rock0910 <k45672204@gmail.com>
2020-06-05 15:11:38 +0800

    Updated

--- htdocs/js/bane.js
+++ htdocs/js/bane.js
@@ -4,44 +4,52 @@ window.addEventListener("load",()=>
 {
 	let html=document.querySelector("html");
 	html.style.backgroundColor = "black";
+	html.style.overflow = "hidden";
 	
-	
-	let scrollStyle = document.createElement("scrollStyle");
+	let scrollStyle = document.createElement("style");
 	scrollStyle.type = "text/css";
-	scrollStyle.innerHTML = ".scrollStyle::-webkit-scrollbar {display:none;}";
+	scrollStyle.innerHTML = "html::-webkit-scrollbar {display:none;}";
 	document.head.appendChild(scrollStyle);
-	html.className = "scrollStyle";
-
-	//let scrollbar =window.getComputedStyle(html,"::-webkit-scrollbar");
 	
+	//let scrollbar =window.getComputedStyle(html,"::-webkit-scrollbar");
 	let body = document.body;
 	body.style.height = "100vh";
-	body.style.width = "100vh";
+	body.style.width = "100vw";
 	body.style.margin = "0";
 	
 	let topCtn = genElement("topCtn","div");
+	topCtn.style.paddingLeft = "3vw";
 	topCtn.style.height = "10vh";
-	topCtn.style.width = "100vh";
+	topCtn.style.width = "97vw";
 	topCtn.style.display = "flex";
-	topCtn.style.justifyContent = "center";
+	topCtn.style.justifyContent = "space-between";
 	topCtn.style.alignItems = "center";
 	topCtn.style.margin = "auto";
 	//topCtn是畫面最上方的物品容器，然後調整他的style屬性
 	
 	let topBtn = genElement("topBtn","button");
 	topBtn.innerText = "≡";
-	topBtn.style.fontSize = "4vh";
+	topBtn.style.display = "flex";
+	topBtn.style.justifyContent = "center";
+	topBtn.style.marginLeft = "2.5vw";
+	topBtn.style.fontSize = "30px";
 	topBtn.style.textAlign = "center";
 	topBtn.style.height = "5vh";
-	topBtn.style.width = "5vh";
+	topBtn.style.width = "5vw";
 	//topBtn是在topCtn裡面的一個按鈕，改了裡面的字等屬性
 	
 	let topTitle = genElement("topTitle","h1");
 	topTitle.innerText = "這是啥小阿~~~~";
 	topTitle.style.color = "white";
-	body.appendChild(topCtn);
+
+	let topBlock = genElement("topBlock","div");
+	topBlock.style.width = "5vw";
+	topBlock.style.marginright = "2.5vw";
+	
 	topCtn.appendChild(topBtn);
 	topCtn.appendChild(topTitle);
+	topCtn.appendChild(topBlock);
+	body.appendChild(topCtn);
 	//topTitle是在topCtn裡面的文字，使用了h1的屬性
 	
 	topBtn.addEventListener("click",()=>
@@ -57,8 +65,9 @@ window.addEventListener("load",()=>
 	});
 	
 	let bodyLayout = genElement("bodyLayout","div");
+	bodyLayout.style.paddingLeft = "3vw";
 	bodyLayout.style.height = "90vh";
-	bodyLayout.style.width = "100vh";
+	bodyLayout.style.width = "100vw";
 	//bodyLayout.style.display = "flex";
 	//bodyLayout.style.justifyContent = "center";
 	//bodyLayout.style.alignItems = "center";
@@ -79,8 +88,9 @@ function genMenu(){
 	let navMenu = genElement("navMenu","nav");
 	navMenu.style.backgroundColor = "gray";
 	navMenu.style.color = "white";
-	navMenu.style.height = "vh";
+	navMenu.style.height = "85vh";
 	navMenu.style.width = "10vw";
+	navMenu.style.outlineWidth = "1vh";
 	let a = document.createElement("ul");
 	a.style.padding = "1vw";
 	a.style.paddingTop = "2vh";

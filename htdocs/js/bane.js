e2685acfded325679026b28e89444865f02c6b41
Rock0910 <30795458+Rock0910@users.noreply.github.com>
2020-06-08 00:15:07 +0800

    MainBoard

--- htdocs/js/bane.js
+++ htdocs/js/bane.js
@@ -6,10 +6,12 @@ window.addEventListener("load",()=>
 	html.style.backgroundColor = "black";
 	html.style.overflow = "hidden";
 	
+	let styleList = document.createElement("div");
 	let scrollStyle = document.createElement("style");
 	scrollStyle.type = "text/css";
 	scrollStyle.innerHTML = "html::-webkit-scrollbar {display:none;}";
-	document.head.appendChild(scrollStyle);
+	document.head.appendChild(styleList);
+	styleList.appendChild(scrollStyle);
 	
 	//let scrollbar =window.getComputedStyle(html,"::-webkit-scrollbar");
 	let body = document.body;
@@ -19,8 +21,9 @@ window.addEventListener("load",()=>
 	
 	let topCtn = genElement("topCtn","div");
 	topCtn.style.paddingLeft = "3vw";
+	topCtn.style.paddingRight = "3vw";
 	topCtn.style.height = "10vh";
-	topCtn.style.width = "97vw";
+	topCtn.style.width = "94vw";
 	topCtn.style.display = "flex";
 	topCtn.style.justifyContent = "space-between";
 	topCtn.style.alignItems = "center";
@@ -65,9 +68,12 @@ window.addEventListener("load",()=>
 	});
 	
 	let bodyLayout = genElement("bodyLayout","div");
+	//bodyLayout.style.backgroundColor = "white";
 	bodyLayout.style.paddingLeft = "3vw";
 	bodyLayout.style.height = "90vh";
 	bodyLayout.style.width = "100vw";
+	bodyLayout.style.display = "flex";
+	bodyLayout.style.flexDirection = "row";
 	//bodyLayout.style.display = "flex";
 	//bodyLayout.style.justifyContent = "center";
 	//bodyLayout.style.alignItems = "center";
@@ -75,6 +81,15 @@ window.addEventListener("load",()=>
 	body.appendChild(bodyLayout);
 	
 	let navMenu =genMenu();
+	
+	let mainBoard = genElement("mainBoard","div");
+	mainBoard.style.backgroundColor = "gray";
+	mainBoard.style.height = "85vh";
+	mainBoard.style.width = "80vw";
+	mainBoard.style.marginLeft = "3vw";
+	mainBoard.style.marginRight = "3vw";
+	
+	bodyLayout.appendChild(mainBoard);
 });
 
 function genElement(x,y){
@@ -86,32 +101,60 @@ function genElement(x,y){
 
 function genMenu(){
 	let navMenu = genElement("navMenu","nav");
+	navMenu.style.display = "flex";
+	navMenu.style.flexDirection = "column";
+	navMenu.style.justifyContent = "space-between";
+	navMenu.style.alignContent = "space-between";
 	navMenu.style.backgroundColor = "gray";
 	navMenu.style.color = "white";
 	navMenu.style.height = "85vh";
 	navMenu.style.width = "10vw";
 	navMenu.style.outlineWidth = "1vh";
+	
 	let a = document.createElement("ul");
-	a.style.padding = "1vw";
-	a.style.paddingTop = "2vh";
+	a.style.display = "flex";
+	a.style.justifyContent = "center";
+	a.style.flexDirection = "column";
+	a.style.padding = "0vw";
+	//a.style.paddingTop = "2vh";
+	a.style.fontSize = "2vw";
 	
 	let b = document.createElement("div");
-	let ba = document.createElement("a");
+	b.innerText="Chat";
+	b.style.color = "inherit";
+	b.style.textDecoration = "underline";
+	b.style.textAlign = "center";
+	b.style.fontSize = "2vw";
+	/*let ba = document.createElement("a");
 	ba.innerText="Chat";
 	ba.style.color = "inherit";
 	ba.href = "#Chat";
-	b.appendChild(ba);
+	b.appendChild(ba);*/
 	
 	let c = document.createElement("div");
-	let ca = document.createElement("a");
+	c.innerText="Paint";
+	c.style.color = "inherit";
+	c.style.textDecoration = "underline";
+	c.style.textAlign = "center";
+	/*let ca = document.createElement("a");
 	ca.innerText="Paint";
 	ca.style.color = "inherit";
 	ca.href = "#Paint";
-	c.appendChild(ca);
+	c.appendChild(ca);*/
+	
+	let d = document.createElement("div");
+	d.innerText="Setting";
+	d.style.color = "inherit";
+	d.style.textDecoration = "underline";
+	d.style.textAlign = "center";
+	d.style.padding = "1vw";
+	d.style.paddingBottom = "4vh";
+	d.style.fontSize = "2vw";
 	
 	a.appendChild(b);
 	a.appendChild(c);
 	navMenu.appendChild(a);
+	navMenu.appendChild(d);
 	bodyLayout.appendChild(navMenu);
 	return navMenu;
 }

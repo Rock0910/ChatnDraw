20a5e482d1db041762904fe1343acdefbe004355
Rock0910 <30795458+Rock0910@users.noreply.github.com>
2020-07-02 21:04:02 +0800

    Using WS

--- httpd/index.js
+++ httpd/index.js
@@ -5,5 +5,19 @@
 	HTTPD.listen(8081);
 	console.log("Come to localhost:8081");
 	
-	const io = require('socket.io');
+	
+	
+	const WebSocket	= require('ws');
+	const wss = new WebSocket.Server({port:8080});
+	wss.on('connection',function connection(ws){
+		console.log('CONNECTED A SHET');
+		
+		ws.on('close',()=>{
+		console.log('CLOSED');
+		});
+		
+		ws.on('message',function incoming(message){
+		console.log(message);
+		});
+	});
 })();

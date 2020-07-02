101f8a5e899f1783cfc143b4991b39b44ad8299c
Rock0910 <30795458+Rock0910@users.noreply.github.com>
2020-07-02 19:02:33 +0800

    CANVAS YEAH

--- httpd/index.js
+++ httpd/index.js
@@ -2,8 +2,8 @@
 
 (() => {
 	const HTTPD = require('./js/httpd').config('./config.json');
-	
 	HTTPD.listen(8081);
-	
 	console.log("Come to localhost:8081");
+	
+	const io = require('socket.io');
 })();
